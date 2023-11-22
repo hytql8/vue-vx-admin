b7308c03802e16e133d7fc85141140c06e8c0da3
花子 <1711034994@qq.com>
2023-11-22 21:41:13 +0800

    wip: 构建中

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="vx-main">
+    <span>5</span>
+    <span>3</span>
+    <span>1</span>
+  </div>
+</template>
+<style lang="scss">
+@import "./styles/namespace.scss";
+.#{$vx-prefix}-main {
+  @include flex(start);
+
+  color: var(--theme-color);
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue"
+import App from "./App.vue"
+// 引入样式
+import "./styles/reset.scss"
+import "./styles/index.scss"
+
+createApp(App).mount("#app")

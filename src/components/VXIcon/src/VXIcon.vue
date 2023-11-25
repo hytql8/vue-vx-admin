cd46e3c81b25d0f66c412d8ef5b8d831eebfb0e8
花子 <1711034994@qq.com>
2023-11-25 20:17:57 +0800

    wip(VXAdmin): 构建中--iconify组件

---
+++ src/components/VXIcon/src/VXIcon.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { h, defineComponent, PropType, computed } from "vue"
+import { Icon } from "@iconify/vue"
+
+export default defineComponent({
+  name: "VXIcon",
+  components: { Icon },
+  props: {
+    icon: {
+      type: String as PropType<string>,
+      default: ""
+    },
+    color: {
+      type: String as PropType<string>
+    },
+    size: {
+      type: Number as PropType<number>,
+      default: 16
+    },
+    hoverColor: {
+      type: String as PropType<string>
+    }
+  },
+  setup(props) {
+    const refHoverColor = computed(() => props.hoverColor)
+    return { hc: refHoverColor }
+  },
+  render() {
+    return h(Icon, { icon: this.icon, color: this.color, size: this.size })
+  }
+})
+</script>
+<style lang="scss">
+.iconify {
+  &:hover {
+    color: v-bind(hc) !important;
+  }
+}
+</style>
--- src/layout/src/components/Horizontal.vue
+++ src/layout/src/components/Horizontal.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
 import { useDark, useToggle } from "@vueuse/core"
 import { ElButton } from "element-plus"
+import { VXIcon } from "@/components/VXIcon"
+import { ref } from "vue"
 
 const isDark = useDark({
   storageKey: "isDark"
 })
 
+const hoverColor = ref("#333333")
+
+setTimeout(() => {
+  hoverColor.value = "red"
+  console.log(hoverColor)
+}, 5000)
+
 const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <ElButton @click="toggleDark()">
-    <i inline-block align-middle i="dark:carbon-moon carbon-sun" />
+  <ElButton @click="toggleDark()" type="primary">
+    <VXIcon icon="line-md:account-alert" color="#999999" :size="14" :hover-color="hoverColor" />
 
     <span class="ml-2">{{ isDark ? "Dark" : "Light" }}</span>
   </ElButton>

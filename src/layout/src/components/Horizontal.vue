4ba89363577262b720063df1af085341c2ff6d8c
花子 <1711034994@qq.com>
2023-12-03 22:02:04 +0800

    wip(VXAdmin): 构建中

--- src/layout/src/components/Horizontal.vue
+++ src/layout/src/components/Horizontal.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import { Menu } from "@/components/Menu"
 import { Logo } from "@/components/Logo"
+import { ThemeSwitch } from "@/components/ThemeSwitch"
 </script>
 
 <template>
-  <ElContainer>
-    <ElHeader>
-      <Logo />
+  <ElContainer class="vx-container">
+    <ElHeader class="vx-header">
+      <Logo class="vx-header__logo" />
+      <div class="vx-header__nav">
+        <ThemeSwitch />
+      </div>
     </ElHeader>
-    <ElContainer>
-      <ElAside width="200px">
-        <Menu />
+    <ElContainer class="vx-content">
+      <ElAside class="vx-aside">
+        <Menu class="vx-aside__menu" />
       </ElAside>
-      <ElMain>Main</ElMain>
+      <ElMain class="vx-content__main">Main</ElMain>
     </ElContainer>
   </ElContainer>
 </template>

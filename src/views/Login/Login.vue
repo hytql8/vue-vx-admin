37b9dff47f285288a4843a90a4a33c1e24bc716e
hytql8 <1711034994@qq.com>
2024-01-12 19:05:29 +0800

    feat(Login): 登录页面开始

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts"></script>
 <template>
-  <div>Login</div>
+  <div class="vx-login">
+    <div class="vx-login__left">
+      <img src="@/assets/svgs/login-banner.svg" alt="" />
+    </div>
+    <div class="vx-login__right">
+      <div class="vx-login__right__login-box"></div>
+    </div>
+  </div>
 </template>
+<style lang="scss">
+@import "./Login.scss";
+</style>

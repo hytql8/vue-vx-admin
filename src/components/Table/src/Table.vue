292bf7bc90448adbd939849bc969b568ad059c7d
hytql8 <1711034994@qq.com>
2024-01-25 19:00:48 +0800

    wip(VxTable): Table组件持续集成

--- src/components/Table/src/Table.vue
+++ src/components/Table/src/Table.vue
@@ -2,7 +2,6 @@
 import { ref, unref, nextTick, computed } from "vue"
 import { ElTable, ElTableColumn } from "element-plus"
 import type { TableParameterTypes } from "./types"
-import { table } from "console"
 defineOptions({
   name: "VxTable"
 })
@@ -41,12 +40,28 @@ const elTableRef = ref<InstanceType<typeof ElTable>>()
 
 const data = computed(() => props.data)
 const columns = computed(() => props.columns)
+const border = computed(() => props.border)
+const rowKey = computed(() => props.rowKey)
+console.log(unref(data), unref(columns), "=============")
+
+console.log(unref(columns)[6].slots)
 </script>
 <template>
   <div>Searchform</div>
   <div>
-    <ElTable :data="data">
-      <ElTableColumn v-for="v in columns" :key="v.id" :field="field" :label="label" :prop="prop" :width="width"></ElTableColumn>
+    <!-- v-bind="$attrs" 可以将vxTable组件身上的事件透传到ElTable上(父组件传入且未定义为props的属性可以透传) -->
+    <ElTable ref="elTableRef" v-bind="$attrs" :data="data" :border="border" :row-key="rowKey">
+      <!-- 默认插槽 -->
+      <slot />
+      <template v-for="v in columns" :key="v.field || v.label">
+        <!-- selection || radio || index || expand || sort -->
+        <ElTableColumn v-if="v.type && v.type == 'selection'" type="selection" v-bind="v"> </ElTableColumn>
+        <ElTableColumn v-else-if="v.type && v.type == 'index'" type="index" v-bind="v"> </ElTableColumn>
+        <!-- 普通列 -->
+        <ElTableColumn v-else v-bind="v">
+          <!-- <template v-for="slot in v.slots" #[slot]="scope"> <slot :name="slot" v-bind="scope" /> </template> -->
+        </ElTableColumn>
+      </template>
     </ElTable>
   </div>
   <div>pagenation</div>

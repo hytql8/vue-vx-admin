26994af8e485855fb483175e9790096940e3c5ca
hytql8 <1711034994@qq.com>
2024-02-01 19:14:22 +0800

    wip(VxTable): Table组件持续集成

--- mock/useTable/index.mock.ts
+++ mock/useTable/index.mock.ts
@@ -1,37 +1,30 @@
 import { MockMethod } from "vite-plugin-mock"
 import Mock from "mockjs"
-Mock.setup({
-  timeout: "1500-3000"
-})
-export default [
-  // useTable list数据
-  {
-    url: "/userTable/list",
-    method: "post",
-    response: () => {
-      // 使用 Mock.js 生成虚拟数据
-      const data = Mock.mock({
-        "list|50": [
-          {
-            "id|+1": 10000,
-            name: () => Mock.Random.cname(),
-            "role|1": ["Develop", "Test", "PM"],
-            "sex|1": ["Man", "Women"],
-            "age|20-40": 1,
-            address: () => Mock.Random.county(true)
-          }
-        ]
-      })
+import { resultPageSuccess } from "../utils"
 
-      return {
-        data: {
-          code: 200,
-          data: {
-            total: data.list.length,
-            list: data.list
-          }
-        }
-      }
+const demoList = (() => {
+  const result: any[] = []
+  for (let index = 0; index < 60; index++) {
+    result.push({
+      "id|+1": 10000,
+      name: () => Mock.Random.cname(),
+      "role|1": ["Develop", "Test", "PM"],
+      "sex|1": ["Man", "Women"],
+      "age|20-40": 1,
+      address: () => Mock.Random.county(true)
+    })
+  }
+  return result
+})()
+
+export default [
+  {
+    url: "/useTable/list",
+    timeout: 2000,
+    method: "post",
+    response: ({ query }) => {
+      const { page = 1, pageSize = 10 } = query
+      return resultPageSuccess(page, pageSize, demoList)
     }
   }
 ] as MockMethod[]
--- src/views/Dashboard/Workplace.vue
+++ src/views/Dashboard/Workplace.vue
@@ -1,5 +1,5 @@
 <script setup lang="tsx">
-import { ref } from "vue"
+import { ref, unref } from "vue"
 import { VxContainer } from "@/components/VxContainer"
 import { Table } from "@/components/Table"
 import { ElButton, ElTag } from "element-plus"
@@ -14,18 +14,8 @@ interface RowVO {
   address: string
 }
 const randomNumber = Math.floor(Math.random() * 10000) + 1
-axios({
-  method: "post",
-  url: "/userTable/list"
-}).then(res => {
-  console.log(res.data)
-})
-const tableData = [
-  { id: 10001, name: "Test1", role: "Develop", sex: "Man", age: 28, address: "test abc" },
-  { id: 10002, name: "Test2", role: "Test", sex: "Women", age: 22, address: "Guangzhou" },
-  { id: 10003, name: "Test3", role: "PM", sex: "Man", age: 32, address: "Shanghai" },
-  { id: 10004, name: "Test4", role: "Designer", sex: "Women", age: 24, address: "Shanghai" }
-]
+
+let tableData = ref([])
 
 const columns = [
   {
@@ -76,7 +66,7 @@ const columns = [
     prop: "action",
     slots: {
       default: () => {
-        return <ElButton>我是column的插槽</ElButton>
+        return <ElButton type="primary">我是column的插槽</ElButton>
       }
     }
   }
@@ -88,6 +78,17 @@ const rowClick = e => {
 const register = (a, b) => {
   console.log(a, b)
 }
+
+const total = ref(0)
+
+axios({
+  method: "post",
+  url: "/useTable/list"
+}).then(res => {
+  console.log(res.data.result)
+  tableData.value = res.data.result.items
+  total.value = res.data.result.total
+})
 </script>
 <template>
   <VxContainer>
@@ -103,6 +104,9 @@ const register = (a, b) => {
         :columns="columns"
         @register="register"
         row-key="id"
+        :pagination="{
+          total
+        }"
       >
       </Table>
     </div>

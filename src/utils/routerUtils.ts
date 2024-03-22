d05bddce390d8f94b53c88c14fcf9d80118d1a52
hytql8 <1711034994@qq.com>
2024-03-22 15:13:34 +0800

    fixed(router): 🐉修复二级router bug

--- src/utils/routerUtils.ts
+++ src/utils/routerUtils.ts
@@ -6,7 +6,7 @@ import { staticRouter } from "@/router"
 import { menuWhiteList } from "@/constants"
 
 const Layout = () => import("@/layout/src/index.vue")
-const secLayout = () => ""
+const secLayout = () => "" as unknown as RouteComponent
 const modules = import.meta.glob("../views/**/*.{vue,tsx}")
 
 // 处理静态路由，降级，keepalive最多只支持缓存二级, 此方法直接使用forEach直接改变传入的routes
@@ -204,19 +204,16 @@ const generateDynamicRouters = (routers: RouteRecordRaw[], mode: RouterMode, use
 // 前端不筛选权限
 const traverseRouting = (routers: RouteRecordRaw[]): RouteRecordRaw[] => {
   for (let v of routers) {
-    // if (v.component) {
     let component = v.component as any
     const module = modules[`${component.replace("/", "../")}.vue`] || modules[`${component.replace("/", "../")}.tsx`]
 
     if (!module && !component.toLowerCase().includes("layout")) {
       console.error(`未找到${component}.vue文件或${component}.tsx文件，请创建`)
     }
-    v.component =
-      component === "layout" ? Layout : component === "secLayout" ? (secLayout() as unknown as RouteComponent) : module
+    v.component = component === "layout" ? Layout : component === "secLayout" ? secLayout() : module
     if (v.children && v.children.length) {
       traverseRouting(v.children)
     }
-    // }
   }
   return routers
 }
@@ -224,7 +221,6 @@ const traverseRouting = (routers: RouteRecordRaw[]): RouteRecordRaw[] => {
 const traverseRoleRouting = (routers: RouteRecordRaw[], roles: string[]): RouteRecordRaw[] => {
   const toolArray = [] as RouteRecordRaw[]
   for (let v of routers) {
-    // if (v.component) {
     let component = v.component as any
     const module = modules[`${component.replace("/", "../")}.vue`] || modules[`${component.replace("/", "../")}.tsx`]
 
@@ -238,8 +234,7 @@ const traverseRoleRouting = (routers: RouteRecordRaw[], roles: string[]): RouteR
         toolArray.push(v)
       }
     }
-    v.component =
-      component === "layout" ? Layout : component === "secLayout" ? (secLayout() as unknown as RouteComponent) : module
+    v.component = component === "layout" ? Layout : component === "secLayout" ? secLayout() : module
     if (v.children && v.children.length) {
       traverseRouting(v.children)
     }
@@ -247,7 +242,6 @@ const traverseRoleRouting = (routers: RouteRecordRaw[], roles: string[]): RouteR
     if (!v.meta?.role && !v.meta?.role?.length) {
       toolArray.push(v)
     }
-    // }
   }
   return toolArray
 }
